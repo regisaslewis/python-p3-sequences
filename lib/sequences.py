677a3df90e6d28b4b130a3a1861f7f463b0486c2
RegisLewis <regisaslewis@gmail.com>
2023-05-31 21:21:22 -0700

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    if int(length) < 1:
+        print(fib_list)
+    elif int(length) < 2:
+        fib_list = [0]
+        print(fib_list)
+    else:
+        fib_list = [0, 1]
+        for i in range(length - 2):
+            fib_list.append(fib_list[-1] + fib_list[-2])
+        print(fib_list)
